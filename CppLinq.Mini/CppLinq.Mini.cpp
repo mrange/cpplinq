a9011697d10eac24ed010dbea26d74180e8bc13c
mrange <marten_range@hotmail.com>
2012-10-20 12:10:16 +0200

    Added CppLinq.Mini in order to be able to detect if just using a bare minimum of cpplinq adds any cost in terms of binary size or compile time

---
+++ CppLinq.Mini/CppLinq.Mini.cpp
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+#include "stdafx.h"
+// ----------------------------------------------------------------------------------------------
+#include "../CppLinq/cpplinq.hpp"
+// ----------------------------------------------------------------------------------------------
+int main (int argc, char* args[])
+{
+    using namespace cpplinq;
+    auto begin = args;
+    auto end = args + argc;
+
+    auto result = from_iterators (begin, end)
+        >> skip (1)
+        >> select ([] (char * cs) {return strlen (cs);})
+        >> sum ()
+        ;
+
+    printf ("%d\r\n", result);
+
+	return 0;
+}
+// ----------------------------------------------------------------------------------------------
