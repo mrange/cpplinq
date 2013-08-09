dd3b8a9affda24bdbd9572462b646f3b010c799c
mrange <marten_range@hotmail.com>
2013-08-09 20:47:50 +0200

    1. Code style updated

--- CppLinq/cpplinq.hpp
+++ CppLinq/cpplinq.hpp
@@ -130,7 +130,7 @@ namespace cpplinq
         {
             enum
             {
-                size = Size,
+                size = Size ,
             };
 
             typedef typename    cleanup_type<TValue>::type  value_type      ;
@@ -2100,8 +2100,8 @@ namespace cpplinq
             typedef                 TOtherKeySelector           other_key_selector_type ;
             typedef                 TCombiner                   combiner_type           ;
             typedef                 std::multimap<
-                                        other_key_type, 
-                                        typename TOtherRange::value_type
+                                            other_key_type
+                                        ,   typename TOtherRange::value_type
                                         >                       map_type                ;
             typedef     typename    map_type::const_iterator    map_iterator_type       ;
 
